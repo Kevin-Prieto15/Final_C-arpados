44b66411bac4dca7936ac5e6fb166c040def7de2
Zamallita2 <zmn0033124@est.univalle.edu>
2025-05-31 02:44:23 -0400

    Sprint 1
    Se ha añadido el movimiento del player, pruebas de colisiones, mapas básico de pruebas, sprites del PLayer, Animaciones del player y Sonido al camianr

---
+++ Assets/01_Scripts/Player.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [Header("Modelos del michi animadooo")]
+    [SerializeField] private GameObject Front;
+    [SerializeField] private GameObject Back;
+    [SerializeField] private GameObject SideRight;
+    [SerializeField] private GameObject SideLeft;
+
+    private GameObject currentModel;
+    private Animator currentAnimator;
+    private AudioSource stepAudio;
+    private bool wasWalking = false;
+
+    private Vector2 input;
+    private Vector2 lastDirection;
+
+    [Header("Velocidad del michi uwu")]
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        SetDirection("Front");
+        lastDirection = Vector2.down;
+        stepAudio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        bool isWalking = input != Vector2.zero;
+
+        if (isWalking)
+        {
+            lastDirection = input;
+
+            if (input.x > 0)
+                SetDirection("SideRight");
+            else if (input.x < 0)
+                SetDirection("SideLeft");
+            else if (input.y > 0)
+                SetDirection("Back");
+            else if (input.y < 0)
+                SetDirection("Front");
+
+            SetWalking(true);
+        }
+        else
+        {
+            SetWalking(false);
+        }
+
+        if (isWalking && !wasWalking)
+        {
+            stepAudio.Play();
+        }
+        else if (!isWalking && wasWalking)
+        {
+            stepAudio.Stop();
+        }
+
+        wasWalking = isWalking; // Recordamos para la próxima uwu
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * speed * Time.fixedDeltaTime);
+    }
+
+    void SetDirection(string direction)
+    {
+        GameObject newModel = null;
+
+        switch (direction)
+        {
+            case "Front":
+                newModel = Front;
+                break;
+            case "Back":
+                newModel = Back;
+                break;
+            case "SideRight":
+                newModel = SideRight;
+                break;
+            case "SideLeft":
+                newModel = SideLeft;
+                break;
+        }
+
+        // ¡Si ya está activo ese modelo, no lo cambies nyaa~!
+        if (newModel == currentModel)
+            return;
+
+        // Apagar todos los michis primero
+        Front.SetActive(false);
+        Back.SetActive(false);
+        SideRight.SetActive(false);
+        SideLeft.SetActive(false);
+
+        // Activar el nuevo modelo
+        newModel.SetActive(true);
+        currentModel = newModel;
+        currentAnimator = currentModel.GetComponent<Animator>();
+    }
+
+
+    void SetWalking(bool isWalking)
+    {
+        if (currentAnimator != null)
+        {
+            currentAnimator.SetBool("IsWalking", isWalking);
+           
+        }
+    }
+}
