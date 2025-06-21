e1fd497f05ce3f28ce3b12c2f999f92798f7dd17
Carla Andrea Camacho Sanizo <carlaandreacamachosanizo@gmail.com>
2025-06-21 10:24:03 -0400

    Commit_Buho

---
+++ Assets/01_Scripts/OwlProjectile.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class OwlProjectile : MonoBehaviour
+{
+    public float gravityScale = 2f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = gravityScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger con: " + other.name);
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("¡Tocó al jugador con trigger!");
+            Player playerScript = other.GetComponent<Player>();
+            if (playerScript != null)
+            {
+                playerScript.TakeDamage(5f);
+            }
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Ground"))
+        {
+            Debug.Log("Tocó el suelo");
+            Destroy(gameObject);
+        }
+    }
+
+}
